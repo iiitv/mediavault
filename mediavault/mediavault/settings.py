3f56adc16c770da5f9431ff729afdb11b293cc60
singhpratyush <singh.pratyush96@gmail.com>
2016-10-24 20:28:23 +0530

    Added API and REST Token apps in settings

--- mediavault/mediavault/settings.py
+++ mediavault/mediavault/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'web.apps.WebConfig',
+    'api.apps.ApiConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
